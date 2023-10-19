770e4e72ddf9c40b12a32d89c948ff1ce7477e17
informaticaesne <informatica@esne.es>
2023-10-19 15:33:49 +0200

    MenuInicial
    He hecho el menu inicial

---
+++ Assets/MenuInicial.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuInicial : MonoBehaviour
+{
+    public void Jugar()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Salir()
+    {
+        Debug.Log("Salir...");
+        Application.Quit();
+    }
+}
