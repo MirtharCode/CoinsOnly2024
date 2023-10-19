d95aaf63d21b92e4def554af71b57dd6520a0892
informaticaesne <informatica@esne.es>
2023-10-19 15:33:13 +0200

    Update MonkeyDMariano.cs

--- Assets/Scripts/MonkeyDMariano.cs
+++ Assets/Scripts/MonkeyDMariano.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class MonkeyDMariano : MonoBehaviour
 {
+    [SerializeField] public bool a;
+
     // Start is called before the first frame update
     void Start()
     {
