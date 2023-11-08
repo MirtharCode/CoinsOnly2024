97b797cf63761e3469337469deab17b826dc40a8
informaticaesne <informatica@esne.es>
2023-11-08 11:54:45 +0100

    Mejora menu
    Sprite aleatorio añadido al menú

--- Assets/Scripts/FotitoPersonajeMenu.cs
+++ Assets/Scripts/FotitoPersonajeMenu.cs
@@ -1,23 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FotitoPersonajeMenu : MonoBehaviour
 {
-    public Sprite[] sprites;
+    public Sprite[] imagenes; // Arrastra tus sprites aquí desde el Inspector.
 
+    private Image imageComponent;
 
     void Start()
     {
-        int randomIndex = Random.Range(0, sprites.Length);
+        // Accede al componente Image.
+        imageComponent = GetComponent<Image>();
 
-        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
-        spriteRenderer.sprite = sprites[randomIndex];
+        if (imagenes.Length > 0)
+        {
+            // Genera un índice aleatorio basado en el número de sprites disponibles.
+            int randomIndex = Random.Range(0, imagenes.Length);
 
+            // Asigna el sprite aleatorio al componente Image.
+            imageComponent.sprite = imagenes[randomIndex];
+        }
+        else
+        {
+            Debug.LogError("No se han asignado sprites en el Inspector.");
+        }
     }
 
-    void Update()
-    {
-        
-    }
 }
