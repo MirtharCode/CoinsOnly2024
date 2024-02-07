1447f0c10a57d00c9168c68b4eefa3651aa96516
informaticaesne <informatica@esne.es>
2024-02-07 20:47:20 +0100

    Inicio Traducción
    Un pequeño paso para el hombre, un gran mgrmgrgmgrl para un múrloc

--- Assets/Scripts/Data.cs
+++ Assets/Scripts/Data.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class Data : MonoBehaviour
 {
+    [SerializeField] public bool spaSpain;
+    [SerializeField] public bool engAmerican;
+    [SerializeField] public bool spaColombian;
+
+    [SerializeField] public GameObject gameManager;
+
     [SerializeField] public int numEvilWizard;
     //[SerializeField] public int numEvilWizardGerard;
     //[SerializeField] public int numEvilWizardManolo;
@@ -81,6 +88,8 @@ public class Data : MonoBehaviour
     [SerializeField] public bool giftRaven = false;         // Te da un disco.
     //[SerializeField] public bool giftRustica = false;
 
+    [SerializeField] public List<string> dialogue;
+
 
     public static Data instance;
 
@@ -88,7 +97,51 @@ public class Data : MonoBehaviour
     {
         if (instance != null) Destroy(gameObject);
         else instance = this;
-
         DontDestroyOnLoad(gameObject);
+
+
     }
+
+    #region Todos los diálogos de Coins Only
+
+    public void SettingDialogues()
+    {
+        Scene currentScene;
+        currentScene = SceneManager.GetActiveScene();
+
+        List<GameObject> _dC;
+        gameManager = GameObject.FindGameObjectWithTag("GM");
+
+        if (spaSpain)
+        {
+            if (currentScene.name == "Day1")
+            {
+                _dC = gameManager.GetComponent<GameManager>().dailyCustomers;
+                List<string> _dC1 = _dC[0].GetComponent<Client>().dialogue;
+                List<string> _dC2 = _dC[1].GetComponent<Client>().dialogue;
+                List<string> _dC3 = _dC[2].GetComponent<Client>().dialogue;
+                List<string> _dC4 = _dC[3].GetComponent<Client>().dialogue;
+                List<string> _dC5 = _dC[4].GetComponent<Client>().dialogue;
+                List<string> _dC6 = _dC[5].GetComponent<Client>().dialogue;
+                List<string> _dC7 = _dC[6].GetComponent<Client>().dialogue;
+                List<string> _dC8 = _dC[7].GetComponent<Client>().dialogue;
+                List<string> _dC9 = _dC[8].GetComponent<Client>().dialogue;
+
+                _dC1.Add("Buenas ciudadano, ya llegó aquí, el inigualable Geeraard, gracias, gracias…");
+                _dC1.Add("…");
+                _dC1.Add("¿Por qué no has empezado a llorar de la alegría y a pedirme un autógrafo mientras estás de rodillas?");
+                _dC1.Add("¡Cómo que no me conoces! Todos aquí me conocen.");
+                _dC1.Add("El héroe de héroes, quien derrotó al Rey Demonio con una sola daga y los ojos vendados.");
+                _dC1.Add("Vamos… Todos mis admiradores, es decir, todo el reino, saben que soy el mejor héroe de la historia.");
+                _dC1.Add("...");
+                _dC1.Add("Bueno, me imagino que perdonaré tu desconocimiento y ese silencio incómodo que provocas.");
+                _dC1.Add("Cóbrame esto al menos, así esta charla dejará de ser tan incómoda.");
+
+                _dC1.Add("Deberías de leer alguna de mis grandes historias humano, adiós.");
+                _dC1.Add("Pero, ¡QUÉ INSOLENCIA ES ESTA! Espero que cuando vuelva aprendas a contar monedas humano.");
+            }
+        }
+    }
+
+    #endregion
 }
