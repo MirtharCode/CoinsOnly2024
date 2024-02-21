e214dc8c538e5044fc18d8901d383c57abc6f811
MirtharCode <80948038+MirtharCode@users.noreply.github.com>
2024-02-21 23:50:29 +0100

    He descomentado cosas de Dia 1 y 2

--- Assets/Scripts/Gramola.cs
+++ Assets/Scripts/Gramola.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
-using UnityEditor.Experimental.GraphView;
+//using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.UI;
 
