0699fe4fbefdc2e2dce02d86638f88bb2e2a0441
informaticaesne <informatica@esne.es>
2024-04-03 08:45:10 +0200

    Geraaaarld en el día 4 cuando viene a regañarte hecho

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -253,16 +253,16 @@ public class GameManager : MonoBehaviour
     {
         canvas.GetComponent<UIManager>().LaVoluntad(50);
         dailyCustomers.Clear();
-        //dailyCustomers.Add(jefe);
-        //dailyCustomers.Add(evilWizardElidora);
-        //dailyCustomers.Add(elementalRockon);
-        //if (!Data.instance.borrachoTriste) dailyCustomers.Add(hybridElvog);
-        //if (Data.instance.borrachoTriste) dailyCustomers.Add(hybridPetra);
-        //dailyCustomers.Add(limbasticCululu);
-        //dailyCustomers.Add(electropedMasermati);
-        //if (Data.instance.vecesCobradoAntonio >= 2) dailyCustomers.Add(limbasticAntonio);
-        //dailyCustomers.Add(evilWizardManoloMano);
-        //dailyCustomers.Add(hybridSaltaralisis);
+        dailyCustomers.Add(jefe);
+        dailyCustomers.Add(evilWizardElidora);
+        dailyCustomers.Add(elementalRockon);
+        if (!Data.instance.borrachoTriste) dailyCustomers.Add(hybridElvog);
+        if (Data.instance.borrachoTriste) dailyCustomers.Add(hybridPetra);
+        dailyCustomers.Add(limbasticCululu);
+        dailyCustomers.Add(electropedMasermati);
+        if (Data.instance.vecesCobradoAntonio >= 2) dailyCustomers.Add(limbasticAntonio);
+        dailyCustomers.Add(evilWizardManoloMano);
+        dailyCustomers.Add(hybridSaltaralisis);
         dailyCustomers.Add(electropedRaven);
         dailyCustomers.Add(elementalTapicio);
         dailyCustomers.Add(detective);
