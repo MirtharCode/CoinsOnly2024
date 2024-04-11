c589e22759d5c1de24b82152fa4b8301d5a8c070
informaticaesne <informatica@esne.es>
2024-04-11 16:37:43 +0200

    Anim mago hielo terminada

--- Assets/Scripts/Herencia/MO_MagoHielo.cs
+++ Assets/Scripts/Herencia/MO_MagoHielo.cs
@@ -9,8 +9,10 @@ public class MO_MagoHielo : MagosOscuros
     [SerializeField] public GameObject product1;
     [SerializeField] public GameObject product2;
     [SerializeField] public GameObject product3;
+    [SerializeField] public GameObject perberto2;
 
     [SerializeField] AudioClip risa;
+    [SerializeField] AudioClip grito;
 
     protected override void Start()
     {
@@ -45,5 +47,22 @@ public class MO_MagoHielo : MagosOscuros
     {
         GetComponent<AudioSource>().clip = risa;
         GetComponent<AudioSource>().Play();
+    }
+
+    public void MagoHieloGrito()
+    {
+        GetComponent<AudioSource>().clip = grito;
+        GetComponent<AudioSource>().Play();
+    }
+
+    public void CallingForPerverto()
+    {
+        perberto2.SetActive(true);
+        perberto2.GetComponent<Animator>().Play("PervertoJumpScare");
+    }
+
+    public void PosMeMato()
+    {
+        Destroy(gameObject);
     }
 }
