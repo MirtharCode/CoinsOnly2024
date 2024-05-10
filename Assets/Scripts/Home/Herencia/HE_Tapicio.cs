341fabbd571ace2af1fa5aa9bb7fce1553a9494b
MirtharCode <80948038+MirtharCode@users.noreply.github.com>
2024-05-10 21:11:41 +0200

    Cambiadas cosas raras que no permitían la Build

--- Assets/Scripts/Home/Herencia/HE_Tapicio.cs
+++ Assets/Scripts/Home/Herencia/HE_Tapicio.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Localization.Plugins.XLIFF.V20;
 using UnityEngine;
 
 public class HE_Tapicio : HomeElementals
--- Assets/Scripts/Home/Transiciones.cs
+++ Assets/Scripts/Home/Transiciones.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class Transiciones : MonoBehaviour
 {
