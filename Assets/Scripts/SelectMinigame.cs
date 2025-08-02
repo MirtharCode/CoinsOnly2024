08edfc54bb7812cce1486b4d6c49e294ad39f4f2
SamuSamuV <samusamu_cb@hotmail.com>
2025-08-02 17:13:06 +0200

    Minijuegos desbloqueables

--- Assets/Scripts/SelectMinigame.cs
+++ Assets/Scripts/SelectMinigame.cs
@@ -8,10 +8,69 @@ public class SelectMinigame : MonoBehaviour
 {
     [SerializeField] Button salButton;
     [SerializeField] Button denjiButton;
+    [SerializeField] Button denjiLevel1Button;
+    [SerializeField] Button denjiLevel2Button;
     [SerializeField] Button elidoraButton;
 
     [SerializeField] GameObject denjiLevels;
     [SerializeField] GameObject gamesLevels;
+
+    [SerializeField] public GameObject data;
+
+    private void Start()
+    {
+        data = GameObject.FindGameObjectWithTag("Data");
+
+        Color gray100 = new Color(0.392f, 0.392f, 0.392f, 1f);
+
+        if (!Data.instance.meExplotasteElCulo1 && !Data.instance.samuraiAyudado1)
+        {
+            denjiLevel1Button.GetComponent<Image>().color = gray100;
+            var colors = denjiLevel1Button.colors;
+            colors.normalColor = gray100;
+            colors.highlightedColor = gray100;
+            colors.pressedColor = gray100;
+            colors.selectedColor = gray100;
+            denjiLevel1Button.colors = colors;
+            denjiLevel1Button.interactable = false;
+        }
+
+        if (!Data.instance.meExplotasteElCulo2 && !Data.instance.samuraiAyudado2)
+        {
+            denjiLevel2Button.GetComponent<Image>().color = gray100;
+            var colors = denjiLevel2Button.colors;
+            colors.normalColor = gray100;
+            colors.highlightedColor = gray100;
+            colors.pressedColor = gray100;
+            colors.selectedColor = gray100;
+            denjiLevel2Button.colors = colors;
+            denjiLevel2Button.interactable = false;
+        }
+
+        if (!Data.instance.slimeFail && !Data.instance.slimeFostiados)
+        {
+            elidoraButton.GetComponent<Image>().color = gray100;
+            var colors = elidoraButton.colors;
+            colors.normalColor = gray100;
+            colors.highlightedColor = gray100;
+            colors.pressedColor = gray100;
+            colors.selectedColor = gray100;
+            elidoraButton.colors = colors;
+            elidoraButton.interactable = false;
+        }
+
+        if (!Data.instance.giftTapicio)
+        {
+            salButton.GetComponent<Image>().color = gray100;
+            var colors = salButton.colors;
+            colors.normalColor = gray100;
+            colors.highlightedColor = gray100;
+            colors.pressedColor = gray100;
+            colors.selectedColor = gray100;
+            salButton.colors = colors;
+            salButton.interactable = false;
+        }
+    }
 
     public void GoSalGame()
     {
