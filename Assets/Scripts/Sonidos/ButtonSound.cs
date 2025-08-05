3c77f9545fa76c6f7717790b4de593737d610535
DavidBarriosPecho <fox123susto@gmail.com>
2025-08-05 15:27:08 +0200

    Sonificando Menu WIP

---
+++ Assets/Scripts/Sonidos/ButtonSound.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ButtonSound : MonoBehaviour, IPointerEnterHandler, IPointerClickHandler
+{
+
+    public AudioClip hoverSound;
+    public AudioClip clickSound;
+    public AudioSource audioSource;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (hoverSound != null && audioSource != null)
+            audioSource.PlayOneShot(hoverSound);
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (clickSound != null && audioSource != null)
+            audioSource.PlayOneShot(clickSound);
+    }
+}
